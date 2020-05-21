c0826f69d197ffd3b106a172afcb01d79d625319
Ian Renton <ian@ianrenton.com>
2020-05-21 10:29:17 +0100

    Add better text for some items and a description of why certain items are missing from the table

--- code.js
+++ code.js
@@ -2,6 +2,7 @@
 var DOMAIN = "https://api.hypixel.net";
 var KEY = "INSERT YOUR KEY HERE - OBTAIN BY RUNNING THE /api COMMAND ON HYPIXEL";
 var MAX_QUANTITY_PER_ORDER = 71680;
+var ITEM_NAMES_LOOKUP = new Map();
 
 // Global data storage
 var apiData = {};
@@ -13,6 +14,28 @@ var maxBacklog = 7;
 var sortBySalesBacklog = false;
 var sortByProfitPerItem = false;
 var sortByTotalProfit = true;
+
+// Item name lookups for certain items where just transforming the name
+// to sentence case isn't enough
+ITEM_NAMES_LOOKUP.set('ENCHANTED_CARROT_STICK', 'Enchanted Carrot on a Stick');
+ITEM_NAMES_LOOKUP.set('HUGE_MUSHROOM_1', 'Brown Mushroom Block');
+ITEM_NAMES_LOOKUP.set('HUGE_MUSHROOM_2', 'Red Mushroom Block');
+ITEM_NAMES_LOOKUP.set('ENCHANTED_HUGE_MUSHROOM_1', 'Enchanted Brown Mushroom Block');
+ITEM_NAMES_LOOKUP.set('ENCHANTED_HUGE_MUSHROOM_2', 'Enchanted Red Mushroom Block');
+ITEM_NAMES_LOOKUP.set('SULPHUR', 'Gunpowder');
+ITEM_NAMES_LOOKUP.set('RABBIT', 'Raw Rabbit');
+ITEM_NAMES_LOOKUP.set('ENCHANTED_RABBIT', 'Enchanted Raw Rabbit');
+ITEM_NAMES_LOOKUP.set('RAW_FISH:1', 'Raw Salmon');
+ITEM_NAMES_LOOKUP.set('RAW_FISH:2', 'Clownfish');
+ITEM_NAMES_LOOKUP.set('RAW_FISH:3', 'Pufferfish');
+ITEM_NAMES_LOOKUP.set('INK_SACK:3', 'Cocoa Beans');
+ITEM_NAMES_LOOKUP.set('INK_SACK:4', 'Lapis Lazuli');
+ITEM_NAMES_LOOKUP.set('LOG', 'Oak Log');
+ITEM_NAMES_LOOKUP.set('LOG:1', 'Spruce Log');
+ITEM_NAMES_LOOKUP.set('LOG:2', 'Birch Log');
+ITEM_NAMES_LOOKUP.set('LOG_2:1', 'Dark Oak Log');
+ITEM_NAMES_LOOKUP.set('LOG_2', 'Acacia Log');
+ITEM_NAMES_LOOKUP.set('LOG:3', 'Jungle Log');
 
 // Basic API request function
 function request(endpoint, params, callback) {
@@ -54,23 +77,33 @@ async function handleData(result) {
 	}
 }
 
-// Prettify item name function
+// Prettify item name function. Either looks up the definition in the list
+// if available, or else just sentence cases the name.
 function prettify(string) {
 	var result;
-	var sentence = string.toLowerCase().split("_");
-	for(var i = 0; i< sentence.length; i++){
-		sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+	if (ITEM_NAMES_LOOKUP.has(string)) {
+		result = ITEM_NAMES_LOOKUP.get(string);
+	} else {
+		var sentence = string.toLowerCase().split("_");
+		for(var i = 0; i< sentence.length; i++){
+			sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+		}
+		result = sentence.join(" ");
+		result = result.replace(":", " : ");
+		result = result.replace(" Item", "");
 	}
-	result = sentence.join(" ");
-	result = result.replace(":", " : ");
-	result = result.replace(" Item", "");
 	return result;
 }
 
 // Main function that performs calculations and updates the display
 function updateDisplay() {
-	// First, set up an array to store our calculated data.
+	// First, set up a list to store our calculated data that will
+	// appear in the table
 	var calcData = [];
+	// And lists to store reasons why items were missed from the table
+	var notProfitable = [];
+	var notAffordable = [];
+	var notSellable = [];
 
 	// Iterate over all products...
 	for (id in apiData.products) {
@@ -113,8 +146,16 @@ function updateDisplay() {
 			item.totalProfit = (item.sellPrice - item.buyPrice) * item.maxQuantity
 
 			// Only store the data if the item is profitable, and we can afford at
-			// least one item, and the sales backlog is below our threshold
-			if (item.profitPerItem >= 0.1 && item.maxQuantity > 0 && item.salesBacklog <= maxBacklog) {
+			// least one item, and the sales backlog is below our threshold. Otherwise
+			// add the name of the item to a separate list so we can note at the bottom
+			// of the table why it's not being displayed.
+			if (item.profitPerItem < 0.1) {
+				notProfitable.push(item);
+			} else if (item.maxQuantity <= 0) {
+				notAffordable.push(item);
+			} else if (item.salesBacklog > maxBacklog) {
+				notSellable.push(item);
+			} else {
 				calcData.push(item);
 			}
 		}
@@ -122,13 +163,13 @@ function updateDisplay() {
 
 	// Apply the required sort to the data
 	if (sortBySalesBacklog) {
-		calcData.sort((a, b) => (a.salesBacklog > b.salesBacklog) ? 1 : -1)
+		calcData.sort((a, b) => (a.salesBacklog > b.salesBacklog) ? 1 : -1);
 	} else if (sortByProfitPerItem) {
-		calcData.sort((a, b) => (a.profitPerItem > b.profitPerItem) ? -1 : 1)
+		calcData.sort((a, b) => (a.profitPerItem > b.profitPerItem) ? -1 : 1);
 	} else if (sortByTotalProfit) {
-		calcData.sort((a, b) => (a.totalProfit > b.totalProfit) ? -1 : 1)
+		calcData.sort((a, b) => (a.totalProfit > b.totalProfit) ? -1 : 1);
 	} else {
-		calcData.sort((a, b) => (a.name > b.name) ? 1 : -1)
+		calcData.sort((a, b) => (a.name > b.name) ? 1 : -1);
 	}
 
 	// Create table header. If maxOffers is >1, an extra column is added to show
@@ -154,17 +195,28 @@ function updateDisplay() {
 		rowFields += "<td>" + item.totalProfit.toFixed(0) + "</td>";
 		var row = $('<tr>').html(rowFields);
 
-		// Set class to give red background if the item gives no profit
-		if (item.profit <= 0.0) {
-			row.addClass("noprofit");
-		}
-
 		// Add to table
 	    table.append(row);
 	});
 
 	// Update DOM
 	$('#table').html(table);
+
+	// Create explanation of missing items
+	var missingItemExplanation = '';
+	if (notProfitable.length > 0) {
+		notProfitable.sort((a, b) => (a.profitPerItem > b.profitPerItem) ? -1 : 1);
+		missingItemExplanation += '<p><b>Items excluded from the table because they are not profitable:</b><br/>' + notProfitable.map(function(o) { return (o.name + ' (' + Math.abs(o.profitPerItem).toFixed(1) + ' loss)'); }).join(', ') + '</p>';
+	}
+	if (notAffordable.length > 0) {
+		notAffordable.sort((a, b) => (a.buyPrice > b.buyPrice) ? -1 : 1);
+		missingItemExplanation += '<p><b>Items excluded from the table because you cannot afford one:</b><br/>' + notAffordable.map(function(o) { return (o.name + ' (' + o.buyPrice.toFixed(1) + ' per item)'); }).join(', ') + '</p>';
+	}
+	if (notSellable.length > 0) {
+		notSellable.sort((a, b) => (a.salesBacklog > b.salesBacklog) ? -1 : 1);
+		missingItemExplanation += '<p><b>Items excluded from the table because the sales backlog is too long:</b><br/>' + notSellable.map(function(o) { return (o.name + ' (' + o.salesBacklog.toFixed(1) + ' days)'); }).join(', ') + '</p>';
+	}
+	$('#missingItemExplanation').html(missingItemExplanation);
 }
 
 // Run on startup:
